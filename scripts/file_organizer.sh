b2fc0f4b26d38dde06b3b87ebc3f72b59cd9f535
Michael Napitupulu <asm34686@gmail.com>
2023-07-16 20:51:01 +0700

    file organizer updated

---
+++ scripts/file_organizer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TARGET_DIR="/home/michael/Downloads"
+
+declare -A folders = (
+	["pdf"] = "Documents"
+	["txt"] = "Documents"
+	["jpg"] = "Images"
+	["png"] = "Images"
+	["jpeg"] = "Images"
+)
+
+for file in "$TARGET_DIR"/*;do
+	if [[ -f "$file" ]]; then
+		extension="${file##*.}"
+
+		if [[-v folders["$extension"]]];then
+			folder= "${folders["$extension"]}"
+			mkdir -p "$TARGET_DIR/$folder"
+
+			mv "$file" "$TARGET_DIR/$folder"
+
+			echo "Moved $file to $TARGET_DIR/$folder"
+		fi
+	fi
+done
+
+
+
