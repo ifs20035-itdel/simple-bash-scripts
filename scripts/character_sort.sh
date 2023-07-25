14e4084974894017304eabc2338dbf72b89ffb30
Michael Napitupulu <asm34686@gmail.com>
2023-07-25 14:13:19 +0700

    leson today

---
+++ scripts/character_sort.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This command below will print 1 to 10 with each of the iteration increment 2 from the last 
+echo {1..10..2}
+
+# This command below will print A to Z in alphabetical order.
+
+echo {A..Z}
+
+# This command below will print a to Z in the ASCII table.
+echo {a..Z}
+
+# This command below will print A to z with the sign_character 
+echo {A..z}
+#
+#
---
+++ scripts/file_command.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# print 1 - 10
+echo {1..10}
+
+
+# create 1000 file
+touch file_{1..1000}
+# But if we list all the files, the sorting of folders will not be to our liking in CLI.
+# Instead, we'll need this command below to put pad zero in front of the number.
+touch file_{01..1000}
+
+# Create files with this series of command
+touch file_{michael, banana, albert}_{01..100}_{w..d..2}
+
+
+
+$DESTINATION = '~/Documents'
+# How to copy
+# -v = verbose means it display all the operation that the commands(in this case 'cp') in the CLI
+# The '1' means that the standard output to rewrite into the file success.txt
+# The '2' means that the standart error to rewrite into the file error.txt
+cp -v * $DESTINATION 1>sucess.txt 2>error.txt
